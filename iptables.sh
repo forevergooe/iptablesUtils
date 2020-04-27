371ffca8f4085830e1186d49bb4c1c3fd8d91680
forevergooe <35247288+forevergooe@users.noreply.github.com>
2020-04-27 14:49:16 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# wget  https://raw.githubusercontent.com/arloor/iptablesUtils/master/iptables.sh;bash iptables.sh;rm -f iptables.sh;
+# wget  https://raw.githubusercontent.com/forevergooe/iptablesUtils/master/iptables.sh;bash iptables.sh;rm -f iptables.sh;
 
 red="\033[31m"
 black="\033[0m"
